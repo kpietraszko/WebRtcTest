765624106372fa6ecee8052e95d4e97b25bdd4f5
kpietraszko <krzysztof@pietraszko.cba.pl>
2019-03-10 11:47:46 +0100

    Counter test

--- webRtc/index.js
+++ webRtc/index.js
@@ -9,6 +9,8 @@ const wss = new WebSocket.Server({ port: WebSocketsConfig.wsServerPort });
 
 const peers = {};
 
+var counter = 0;
+
 wss.on('connection', function connection(ws) {
 	var peer = new Peer({
 		initiator: true, wrtc: wrtc, trickle: false,
@@ -34,7 +36,7 @@ wss.on('connection', function connection(ws) {
 		// wait for 'connect' event before using the data channel
 		console.log("WebRTC connection established");
 		setInterval(() =>
-			peer.send(`hey peer #${id}, updated 2019-03-10 11:26`),
+			peer.send(`hey peer #${id}, updated 2019-03-10 11:47, ${counter++}`),
 			1000);
 	})
 });
