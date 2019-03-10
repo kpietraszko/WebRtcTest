6ac1ba11eaaa5e26a017278cc90f740b5a5116f4
kpietraszko <krzysztof@pietraszko.cba.pl>
2019-03-10 15:06:13 +0100

    dito

--- webRtc/index.js
+++ webRtc/index.js
@@ -36,7 +36,7 @@ wss.on('connection', function connection(ws) {
 		// wait for 'connect' event before using the data channel
 		console.log("WebRTC connection established");
 		setInterval(() =>
-			peer.send(`hey peer #${id}, updated 2019-03-10 12:02, ${counter++}`),
+			peer.send(`hey peer #${id}, updated 2019-03-10 15:06, ${counter++}`),
 			100);
 	})
 });
