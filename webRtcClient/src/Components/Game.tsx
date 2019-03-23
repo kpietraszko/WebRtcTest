454b72554e49146c06db076e68ea135b5bfe4907
kpietraszko <krzysztof@pietraszko.cba.pl>
2019-03-23 23:11:58 +0100

    Changed light to directional

--- webRtcClient/src/Components/Game.tsx
+++ webRtcClient/src/Components/Game.tsx
@@ -29,8 +29,8 @@ const Game : FC = () => {
         // This attaches the camera to the canvas
         camera.attachControl(canvas, true);
 
-        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+        // This creates a light
+        var light = new BABYLON.DirectionalLight("light1", new BABYLON.Vector3(0, -1, 0), scene);
 
         // Default intensity is 1. Let's dim the light a small amount
         light.intensity = 0.7;
