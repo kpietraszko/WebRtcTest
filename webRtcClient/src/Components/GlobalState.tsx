7d1e03f47c579c3fe941885898c204fe9364fad3
kpietraszko <krzysztof@pietraszko.cba.pl>
2019-03-23 22:19:22 +0100

    Updating fps every 30 frames

--- webRtcClient/src/Components/GlobalState.tsx
+++ webRtcClient/src/Components/GlobalState.tsx
@@ -9,13 +9,14 @@ export interface Props {
 
 const GlobalStateHandler : FC<Props> = (props) => {
 	const [state, dispatch] = useReducer(genericReducer, { fps: 0 });
-	var framesToUpdateFpsCounter = 60
+	var framesToUpdateFpsCounter = 30
 	
 	const setFps = (fps: number): void => {
 		framesToUpdateFpsCounter--;
 		if (framesToUpdateFpsCounter === 0){
 			dispatch(setFpsActionCreator(fps));
-			framesToUpdateFpsCounter = 10;
+			console.log("Updating fps");
+			framesToUpdateFpsCounter = 30;
 		}
 	}
 
