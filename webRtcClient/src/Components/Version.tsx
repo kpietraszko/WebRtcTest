c425baa131ded544608683037eea728c420cabf5
kpietraszko <krzysztof@pietraszko.cba.pl>
2019-03-23 13:11:30 +0100

    styling build date footer

--- webRtcClient/src/Components/Version.tsx
+++ webRtcClient/src/Components/Version.tsx
@@ -1,9 +1,19 @@
-import { FC } from "react";
+import React, { FC } from "react";
 import preval from 'preval.macro';
+import { style } from "typestyle";
+import { px, em } from "csx";
 
+const footerStyle = style({
+	fontSize: em(3),
+	position: "absolute",
+	bottom: px(10),
+	left: px(10),
+	color: "white",
+	mixBlendMode: "difference"
+});
 const Version: FC = () => {
 	return (
-		<footer>
+		<footer className={footerStyle}>
 			Build date: {preval`module.exports = new Date().toLocaleString();`}.
 		</footer>
 	);
