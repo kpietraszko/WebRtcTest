26a9b0afd158fc8c3c6151f6c3c63bbab525b303
kpietraszko <krzysztof@pietraszko.cba.pl>
2019-03-24 18:01:19 +0100

    Restored PBR for mobile test

--- webRtcClient/src/Components/Game.tsx
+++ webRtcClient/src/Components/Game.tsx
@@ -50,7 +50,11 @@ const Game: FC = () => {
 
         // Our built-in 'sphere' shape.
         const sphere = BABYLON.MeshBuilder.CreateSphere("sphere1", { segments: 16, diameter: 2} , scene);
-        sphere.material = sphereMaterial;
+        const sphereMat = new BABYLON.PBRMetallicRoughnessMaterial("sphereMat", scene);
+        sphereMat.baseColor = new BABYLON.Color3(0.206, 0.94, 1);
+        sphereMat.metallic = 0;
+        sphereMat.roughness = 0.1;
+        sphere.material = sphereMat;
         sphere.material.freeze();
 
         // Move the sphere upward 1/2 its height
@@ -64,8 +68,13 @@ const Game: FC = () => {
         // groundMaterial.wireframe = true;
         // Our built-in 'ground' shape.
         const ground = BABYLON.MeshBuilder.CreateGround("ground1", { width: 6, height: 6, subdivisions: 2}, scene);
+        const groundMat = new BABYLON.PBRMetallicRoughnessMaterial ("groundMat", scene);
+        groundMat.baseColor = Color3.Gray();
+        groundMat.metallic = 0;
+        groundMat.roughness = 0.4;
 
-        ground.material = groundMaterial;
+
+        ground.material = groundMat;
         ground.material.freeze();
         
         ground.freezeNormals();
