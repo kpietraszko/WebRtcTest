4b7d830e7e7ec2af6a89f119587ee43af1603548
kpietraszko <krzysztof@pietraszko.cba.pl>
2019-03-24 15:32:02 +0100

    Switched to PBRMetallicRoughnessMaterial

--- webRtcClient/src/Components/Game.tsx
+++ webRtcClient/src/Components/Game.tsx
@@ -5,6 +5,7 @@ import { style } from "typestyle";
 import { viewWidth, viewHeight, px, percent } from "csx";
 import { SetFpsContext } from "../contexts";
 import Fullscreen from "react-full-screen";
+import { Color3 } from "babylonjs";
 
 const canvasStyle = style({
     width: percent(100),
@@ -23,7 +24,7 @@ const Game: FC = () => {
         // BABYLON.SceneOptimizer.OptimizeAsync(scene);
 
         // This creates and positions a camera (non-mesh)
-        var camera = new BABYLON.ArcRotateCamera("camera1", 1, Math.PI / 3, 8, new BABYLON.Vector3(0, 0, 0), scene); //new BABYLON.Vector3(0, 5, -10), scene);
+        const camera = new BABYLON.ArcRotateCamera("camera1", 1, Math.PI / 3, 8, new BABYLON.Vector3(0, 0, 0), scene); //new BABYLON.Vector3(0, 5, -10), scene);
 
         // This targets the camera to scene origin
         camera.setTarget(BABYLON.Vector3.Zero());
@@ -32,23 +33,35 @@ const Game: FC = () => {
         camera.attachControl(canvas, true);
 
         // This creates a light
-        var light = new BABYLON.DirectionalLight("light1", new BABYLON.Vector3(0, -1, 0), scene);
+        const light = new BABYLON.DirectionalLight("light1", new BABYLON.Vector3(0, -1, 0), scene);
 
-        // Default intensity is 1. Let's dim the light a small amount
-        light.intensity = 0.7;
+        const ambientLight = new BABYLON.HemisphericLight("ambientLight", new BABYLON.Vector3(0, 1, 0), scene);
+        ambientLight.intensity = 0.1;
+
+        // Default intensity is 1.
+        light.intensity = 2;
 
         // Our built-in 'sphere' shape.
-        var sphere = BABYLON.MeshBuilder.CreateSphere("sphere1", { segments: 16, diameter: 2} , scene);
-
-        sphere.material && sphere.material.freeze();
+        const sphere = BABYLON.MeshBuilder.CreateSphere("sphere1", { segments: 16, diameter: 2} , scene);
+        const sphereMat = new BABYLON.PBRMetallicRoughnessMaterial("sphereMat", scene);
+        sphereMat.baseColor = new BABYLON.Color3(0.206, 0.94, 1);
+        sphereMat.metallic = 0;
+        sphereMat.roughness = 0.1;
+        sphere.material = sphereMat;
+        sphere.material.freeze();
 
         // Move the sphere upward 1/2 its height
         sphere.position.y = 1;
 
         // Our built-in 'ground' shape.
-        var ground = BABYLON.MeshBuilder.CreateGround("ground1", { width: 6, height: 6, subdivisions: 2}, scene);
-
-        ground.material && ground.material.freeze();
+        const ground = BABYLON.MeshBuilder.CreateGround("ground1", { width: 6, height: 6, subdivisions: 2}, scene);
+        const groundMat = new BABYLON.PBRMetallicRoughnessMaterial ("groundMat", scene);
+        groundMat.baseColor = Color3.Gray();
+        groundMat.metallic = 0;
+        groundMat.roughness = 0.4;
+        
+        ground.material = groundMat;
+        ground.material.freeze();
         
         ground.freezeNormals();
 
