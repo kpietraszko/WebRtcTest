82a075094254d85414452910fff47bd6d5060d75
kpietraszko <krzysztof@pietraszko.cba.pl>
2019-12-28 20:53:52 +0100

    Added .NET Core solution

---
+++ webRtcNetCore/webRtcNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace webRtcNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
